07faa450162d1d846c9d8d564f572dea79c52978
elmoaid-lgtm <eali@needspec.com>
2026-03-21 06:01:00 -0400

    chore: merge feature branch

--- tests/test_mcp_server.py
+++ tests/test_mcp_server.py
@@ -270,7 +270,7 @@ class TestParameters:
 class TestTokenBudgets:
     def test_overview_cheap(self):
         r = assert_ok(overview(REPO_PATH, output_format="json"))
-        assert r["tokens"] < 1400  # bumped S185-S190+S20: more scenarios + lang handlers
+        assert r["tokens"] < 1550  # bumped S203-S252: continuous signal additions grow repo overview
 
     def test_stats_cheap(self):
         r = assert_ok(stats(REPO_PATH, output_format="json"))
